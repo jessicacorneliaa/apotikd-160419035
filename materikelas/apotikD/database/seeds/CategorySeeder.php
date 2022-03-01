f4bef7fcb5c834ae52b6a45ce59cd3f74e8f65f4
jessicacornelia <s160419035@student.ubaya.ac.id>
2022-03-01 12:20:07 +0700

    week 4(controller,model,view - medicine&category)

--- materikelas/apotikD/database/seeds/CategorySeeder.php
+++ materikelas/apotikD/database/seeds/CategorySeeder.php
@@ -13,9 +13,18 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        DB::table('categories')->insert([
-            'category_name' => 'Alat Kesehatan'
-        ]);
+        DB::table('categories')->insert(['category_name' => 'ANALGESIK NON NARKOTIK', 'description'=>'1.2. ANALGESIK NON NARKOTIK']);
+        DB::table('categories')->insert(['category_name' => 'ANTIPIRAI', 'description'=>'1.3. ANTIPIRAI']);
+        DB::table('categories')->insert(['category_name' => 'ANESTETIK LOKAL', 'description'=>'2.1 ANESTETIK LOKAL']);
+        DB::table('categories')->insert(['category_name' => 'ANTIMIGREN', 'description'=>'7.1 ANTIMIGREN']);
+        DB::table('categories')->insert(['category_name' => 'ANTIVERTIGO', 'description'=>'7.2 ANTIVERTIGO']);
+        DB::table('categories')->insert(['category_name' => 'IMUNOSUPRESAN', 'description'=>'8.2 IMUNOSUPRESAN']);
+        DB::table('categories')->insert(['category_name' => 'SITOTOKSIK', 'description'=>'8.3 SITOTOKSIK']);
+        DB::table('categories')->insert(['category_name' => 'DIURETIK', 'description'=>'15.1 DIURETIK']);
+        DB::table('categories')->insert(['category_name' => 'OBAT untuk HIPERTROFI PROSTAT', 'description'=>'15.2 OBAT untuk HIPERTROFI PROSTAT']);
+        DB::table('categories')->insert(['category_name' => 'HORMON ANTIDIURETIK', 'description'=>'16.1 HORMON ANTIDIURETIK']);
+        DB::table('categories')->insert(['category_name' => 'ANTIDIABETES', 'description'=>'16.2 ANTIDIABETES']);
+        DB::table('categories')->insert(['category_name' => 'TROMBOLITIK', 'description'=>'17.5 TROMBOLITIK']);
 
     }
 }
--- materikelas/apotikD/routes/web.php
+++ materikelas/apotikD/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Medicine
+Route::resource('medicines', 'MedicineController');
+
+// Category
+Route::resource('categories','CategoryController');
